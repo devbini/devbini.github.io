a09b3acf29197fbb06867921a1fba341c5453304
devbini <cong8685@naver.com>
2024-07-31 16:33:37 +0900

    fix: Fixed issue with CSS not loading properly 2024-07-31 basepath 설정을 통한 css 읽기 내용 추가

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,8 @@
 const nextConfig = {
     reactStrictMode: true,
     output: 'export',
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/repository/' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/devbini.github.io/' : '',
+    basePath: process.env.NODE_ENV === 'production' ? '/devbini.github.io' : '',
     trailingSlash: true,
     images: {
         unoptimized: true,
