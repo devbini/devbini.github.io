9f204b7eb9fbacf10562f4d133b8bc3c02a5eaa5
devbini <devbini@users.noreply.github.com>
2024-11-10 19:33:30 +0000

    deploy: 2a3a1b00f000cab2d723ac0d74220aaffddde859

---
+++ _next/static/chunks/app/layout-b9f99192e2d46d93.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{5955:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,72,23))},72:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[985,441,517,358],()=>s(5955)),_N_E=e.O()}]);
