e3dc47bc916caaea9ebed7cfc76c2562fbce142f
devbini <cong8685@naver.com>
2024-11-12 19:18:09 +0900

    hotfix: Rebase Base URL #2

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,9 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repoName = "";
-
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: isGithubActions ? `/${repoName}` : "",
-  assetPrefix: isGithubActions ? `/${repoName}/` : "",
+  basePath: "",
+  assetPrefix: "",
   images: {
     unoptimized: true,
   },
